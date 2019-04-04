5192a11713028d0a203989032cfba2af38633d78
Eugene Strokin <eugene@strokin.info>
2019-04-04 16:50:10 -0400

    Update Test2Script.kts

--- Test2Script.kts
+++ Test2Script.kts
@@ -1,7 +1,7 @@
 import com.comcast.xbo.wpil.CloudFunction
 import com.comcast.xbo.wpil.device.Device
 
-class Test2Script : CloudFunction<Device, Device>{
+class Test2Script : CloudFunction<Device, Device>(){
 
     override fun apply(t: Device): Device {
         println("IN THE SCRIPT $t")
