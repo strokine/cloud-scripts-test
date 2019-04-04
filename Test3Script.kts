a5602aa379215309509936cf5014afce224a2033
Eugene Strokin <eugene@strokin.info>
2019-04-04 17:40:44 -0400

    Update Test3Script.kts

--- Test3Script.kts
+++ Test3Script.kts
@@ -4,7 +4,7 @@ import com.comcast.xbo.wpil.device.Device
 class Test3Script : CloudFunction<Device, Device>(){
 
     override fun apply(t: Device): Device {
-        println("IN THE SCRIPT $t")
+        println("Hello there SCRIPT $t")
         t.name = t.name!!.toUpperCase()
         return t
     }
