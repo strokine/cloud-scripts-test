ed48b9b8716915ebdd4782d7936cf2a540389c51
Eugene Strokin <eugene@strokin.info>
2019-04-04 16:49:27 -0400

    Create Test2Script.kts

---
+++ Test2Script.kts
@@ -0,0 +1,13 @@
+import com.comcast.xbo.wpil.CloudFunction
+import com.comcast.xbo.wpil.device.Device
+
+class Test2Script : CloudFunction<Device, Device>{
+
+    override fun apply(t: Device): Device {
+        println("IN THE SCRIPT $t")
+        t.name = t.name!!.toUpperCase()
+        return t
+    }
+}
+
+Test2Script()
