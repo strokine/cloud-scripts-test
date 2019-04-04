f02f103a609624c6e4e074a6d4e9109c62d0f039
Eugene Strokin <eugene@strokin.info>
2019-04-04 17:45:11 -0400

    Update Test3Script.kts

--- Test3Script.kts
+++ Test3Script.kts
@@ -1,7 +1,7 @@
 import java.util.function.Function
 import com.comcast.xbo.wpil.device.Device
 
-class Test3Script : Function<Device, Device>(){
+class Test3Script : Function<Device, Device>{
 
     override fun apply(t: Device): Device {
         println("Hello there SCRIPT $t")
